084553dd9f7f6a04e8351c05de7db6508a3bddaf
Shahiduzzaman Bhuiyan <bhuiyanshaheduzzaman@gmail.com>
2025-08-04 04:45:03 +0600

    final commit2

--- server.js
+++ server.js
@@ -13,14 +13,11 @@ const app = express();
 app.use(cors()); // Enable CORS for all origins (adjust for production)
 app.use(express.json()); // Parse JSON request bodies
 
-// Routes
+// API Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
-// Basic home route for testing
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
+// Serve static files from the React frontend app
 if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(
@@ -29,6 +26,7 @@ if (process.env.NODE_ENV === "production") {
     )
   );
 
+  
   app.get("*", (req, res) => {
     res.sendFile(
       path.resolve(
@@ -41,11 +39,12 @@ if (process.env.NODE_ENV === "production") {
     );
   });
 } else {
-  // Basic home route for testing in development
+
   app.get("/", (req, res) => {
     res.send("API is running...");
   });
 }
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
