67b79a069b7f1eb4ee50f8f93670724dec71ec4b
Shahiduzzaman Bhuiyan <bhuiyanshaheduzzaman@gmail.com>
2025-08-04 04:56:02 +0600

    final commit3

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV === "production") {
   );
 
   
-  app.get("*", (req, res) => {
+  app.get("/*splat", (req, res) => {
     res.sendFile(
       path.resolve(
         __dirname,
